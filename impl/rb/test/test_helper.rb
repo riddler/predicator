9f6b542e87d7fa7fd483c8e06f903809e4a113ee
JohnnyT <ubergeek3141@gmail.com>
2016-02-21 20:22:19 -0700

    Moves Coveralls before code in test_helper

--- impl/rb/test/test_helper.rb
+++ impl/rb/test/test_helper.rb
@@ -1,6 +1,8 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+
 require "coveralls"
-require "predicator"
 require "minitest/autorun"
 require "minitest/pride"
+
 Coveralls.wear!
+require "predicator"
