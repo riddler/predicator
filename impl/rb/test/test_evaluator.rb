7b0857543f2f8f8c68515ab960fd48ef19937033
JohnnyT <ubergeek3141@gmail.com>
2016-09-10 22:25:30 -0600

    Adds assert_eval and cleans up TestEvaluator

--- impl/rb/test/test_evaluator.rb
+++ impl/rb/test/test_evaluator.rb
@@ -3,124 +3,99 @@ require "helper"
 module Predicator
   class TestEvaluator < Minitest::Test
     def test_true
-      instructions = [{op: "lit", lit: true}]
-      e = Evaluator.new instructions
-      assert_equal true, e.result
+      assert_eval true, [{op: "lit", lit: true}]
     end
 
     def test_false
-      instructions = [{op: "lit", lit: false}]
-      e = Evaluator.new instructions
-      assert_equal false, e.result
+      assert_eval false, [{op: "lit", lit: false}]
     end
 
     def test_integer_equal_integer
-      instructions = [
+      assert_eval true, [
         {op: "lit", lit: 1},
         {op: "lit", lit: 1},
         {op: "compare", comparison: "EQ"},
       ]
-      e = Evaluator.new instructions
-      assert_equal true, e.result
     end
 
     #--- AND
     def test_true_and_true
-      instructions = [
+      assert_eval true, [
         {op: "lit", lit: true},
         {op: "jump_if_false", label: "end_and"},
         {op: "lit", lit: true},
         {op: "label", label: "end_and"},
       ]
-      e = Evaluator.new instructions
-      assert_equal true, e.result
-      assert_empty e.stack
     end
 
     def test_false_and_false
-      instructions = [
+      assert_eval false, [
         {op: "lit", lit: false},
         {op: "jump_if_false", label: "end_and"},
         {op: "lit", lit: false},
         {op: "label", label: "end_and"},
       ]
-      e = Evaluator.new instructions
-      assert_equal false, e.result
-      assert_empty e.stack
     end
 
     def test_false_and_true
-      instructions = [
+      assert_eval false, [
         {op: "lit", lit: false},
         {op: "jump_if_false", label: "end_and"},
         {op: "lit", lit: true},
         {op: "label", label: "end_and"},
       ]
-      e = Evaluator.new instructions
-      assert_equal false, e.result
-      assert_empty e.stack
     end
 
     def test_true_and_false
-      instructions = [
+      assert_eval false, [
         {op: "lit", lit: true},
         {op: "jump_if_false", label: "end_and"},
         {op: "lit", lit: false},
         {op: "label", label: "end_and"},
       ]
-      e = Evaluator.new instructions
-      assert_equal false, e.result
-      assert_empty e.stack
     end
 
     #--- OR
     def test_true_or_true
-      instructions = [
+      assert_eval true, [
         {op: "lit", lit: true},
         {op: "jump_if_true", label: "end_or"},
         {op: "lit", lit: true},
         {op: "label", label: "end_or"},
       ]
-      e = Evaluator.new instructions
-      assert_equal true, e.result
-      assert_empty e.stack
     end
 
     def test_false_or_false
-      instructions = [
+      assert_eval false, [
         {op: "lit", lit: false},
         {op: "jump_if_true", label: "end_or"},
         {op: "lit", lit: false},
         {op: "label", label: "end_or"},
       ]
-      e = Evaluator.new instructions
-      assert_equal false, e.result
-      assert_empty e.stack
     end
 
     def test_false_or_true
-      instructions = [
+      assert_eval true, [
         {op: "lit", lit: false},
         {op: "jump_if_true", label: "end_or"},
         {op: "lit", lit: true},
         {op: "label", label: "end_or"},
       ]
-      e = Evaluator.new instructions
-      assert_equal true, e.result
-      assert_empty e.stack
     end
 
     def test_true_or_false
-      instructions = [
+      assert_eval true, [
         {op: "lit", lit: true},
         {op: "jump_if_true", label: "end_or"},
         {op: "lit", lit: false},
         {op: "label", label: "end_or"},
       ]
-      e = Evaluator.new instructions
-      assert_equal true, e.result
-      assert_empty e.stack
     end
 
+    def assert_eval result, instructions
+      e = Evaluator.new instructions
+      assert_equal result, e.result
+      assert_empty e.stack
+    end
   end
 end
