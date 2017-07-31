ec80a68485ede11e67900bc94a95e911034ea8bb
Catalina GitHub <castengo23@gmail.com>
2017-07-31 13:31:48 -0600

    adds visit array visitor and refactor visitor

--- impl/rb/lib/predicator/visitors/visitor.rb
+++ impl/rb/lib/predicator/visitors/visitor.rb
@@ -15,30 +15,34 @@ module Predicator
         send DISPATCH_CACHE[node.type], node
       end
 
+      def visit_children node
+        node.children.each {|child| visit child}
+      end
+
       def unary node
         visit node.left
       end
-      def visit_NOT node;   unary node; end
-      def visit_GROUP node; unary node; end
+      def visit_NOT node;   visit_children node; end
+      def visit_GROUP node; visit_children node; end
 
       def binary node
         visit node.left
         visit node.right
       end
-      def visit_EQ node;    binary node; end
-      def visit_GT node;    binary node; end
-      def visit_LT node;    binary node; end
-      def visit_AND node;   binary node; end
-      def visit_OR node;    binary node; end
-      def visit_IN node;    binary node; end
-      def visit_NOTIN node; binary node; end
+      def visit_EQ node;    visit_children node; end
+      def visit_GT node;    visit_children node; end
+      def visit_LT node;    visit_children node; end
+      def visit_AND node;   visit_children node; end
+      def visit_OR node;    visit_children node; end
+      def visit_IN node;    visit_children node; end
+      def visit_NOTIN node; visit_children node; end
 
       def ternary node
         visit node.left
         visit node.middle
         visit node.right
       end
-      def visit_BETWEEN node; ternary node; end
+      def visit_BETWEEN node; visit_children node; end
 
       def terminal node; end
       def visit_TRUE node;      terminal node; end
@@ -46,6 +50,10 @@ module Predicator
       def visit_INTEGER node;   terminal node; end
       def visit_STRING node;    terminal node; end
       def visit_VARIABLE node;  terminal node; end
+
+      def visit_ARRAY node
+        node.left.each{ |item| visit item }
+      end
     end
   end
 end
